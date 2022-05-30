c9e084ad6537836acaee4dc46563b2f9c3b1608f
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-30 18:21:30 +0530

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -34,7 +34,7 @@ export default class Command extends BaseCommand {
 		} catch (err) {
 			M.reply(`Profile Picture not Accessible of ${username} must be ugly probably`);
 			pfp =
-				"https://i.ibb.co/x1jcS2w/2557ca680a037ecac23bcea30722136e.jpg";
+				"https://i.ibb.co/zJkfPBB/AE-Entropy-on-Twitter.png";
 		}
 		const exp = (await this.client.getUser(user)).Xp;
 		let role: string;
